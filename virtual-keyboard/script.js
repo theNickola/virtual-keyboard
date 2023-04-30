3aafd4908db08730fad43ca7f4f1c85cffdda4d3
theNickola <kurilchik.nv@gmail.com>
2023-04-30 16:03:02 +0300

    feat: add events keydown and keyup for sym,let,dig

--- virtual-keyboard/script.js
+++ virtual-keyboard/script.js
@@ -6,6 +6,9 @@ class Key {
     this.type = type;
   }
 }
+let arrayKeys = [];
+
+if (localStorage.lang) localStorage.lang = 'EN-en';
 
 const JsonURLKeys = 'assets/keys.json';
 const requestKeys = new XMLHttpRequest();
@@ -36,8 +39,10 @@ requestKeys.onload = () => {
     for (let j = 0; j < oneRowKeys.length; j += 1) {
       const key = document.createElement('div');
       key.classList.add('keyboard__key');
-      if (oneRowKeys[j].isService) key.classList.add('keyboard__key_type_service');
-      switch (oneRowKeys[j].size) {
+      const { code, size, isService } = oneRowKeys[j];
+      key.id = code;
+      if (isService) key.classList.add('keyboard__key_type_service');
+      switch (size) {
         case 's': key.classList.add('keyboard__key_size_s'); break;
         case 'm': key.classList.add('keyboard__key_size_m'); break;
         case 'l': key.classList.add('keyboard__key_size_l'); break;
@@ -45,8 +50,7 @@ requestKeys.onload = () => {
         case 'xxl': key.classList.add('keyboard__key_size_xxl'); break;
         default: break;
       }
-      //TODO: implement to adding current language
-      key.innerHTML = oneRowKeys[j].code;
+      // key.innerHTML = oneRowKeys[j].code;
       keyboardRow.append(key);
     }
     keyboard.append(keyboardRow);
@@ -68,7 +72,10 @@ requestKeys.onload = () => {
     const currentBtn = document.getElementById(e.code);
     if (currentBtn) {
       currentBtn.classList.add('keyboard__key_active');
-      textfield.setRangeText(e.key, textfield.selectionStart, textfield.selectionStart, 'end');
+      const isServiceCurrentBtn = keys.filter((el) => el.code === e.code)[0].isService;
+      if (!isServiceCurrentBtn) {
+        textfield.setRangeText(e.key, textfield.selectionStart, textfield.selectionStart, 'end');
+      }
     }
   });
   document.addEventListener('keyup', (e) => {
@@ -86,7 +93,6 @@ requestKeys.onload = () => {
       textfield.selectionStart,
       textfield.selectionStart,
     );
-
     textfield.selectionStart += currentBtn.innerText.length;
     textfield.focus();
   });
@@ -95,38 +101,3 @@ requestKeys.onload = () => {
     currentBtn.classList.remove('keyboard__key_active');
   });
 };
-
-const arrayOfKeysEN = [
-  [['Backquote', '`', '~', 'sym'], ['Digit1', '1', '!', 'dig'], ['Digit2', '2', '@', 'dig'], ['Digit3', '3', '#', 'dig'], ['Digit4', '4', '$', 'dig'], ['Digit5', '5', '%', 'dig'], ['Digit6', '6', '^', 'dig'], ['Digit7', '7', '&', 'dig'], ['Digit8', '8', '*', 'dig'], ['Digit9', '9', '(', 'dig'], ['Digit0', '0', ')', 'dig'], ['-', '_'], ['=', '+'], ['Backspace']],
-  [['Tab'], ['q', 'Q'], ['w', 'W'], ['e', 'E'], ['r', 'R'], ['t', 'T'], ['y', ''], ['u'], ['i'], ['o'], ['p'], ['['], [']'], ['\\'], ['Del']],
-  [['Caps Lock'], ['a'], ['s'], ['d'], ['f'], ['g'], ['h'], ['j'], ['k'], ['l'], [';'], ['\''], ['Enter']],
-  [['Shift'], ['z'], ['x'], ['c'], ['v'], ['b'], ['n'], ['m'], [','], ['.'], ['/'], ['▲'], ['Shift']],
-  [['Ctrl'], ['Win'], ['Alt'], ['Space'], ['Alt'], ['◄'], ['▼'], ['►'], ['Ctrl']]];
-
-// const renderKeys = () => {
-//   const arrayOfKeysRU = ['RU',
-//     ['ё', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace'],
-//     ['Tab', 'й', 'ц', 'у', 'к', 'е', 'н', 'г', 'ш', 'щ', 'з', 'х', 'ъ', '\\', 'Del'],
-//     ['Caps Lock', 'ф', 'ы', 'в', 'а', 'п', 'р', 'о', 'л', 'д', 'ж', 'э', 'Enter'],
-//     ['Shift', 'я', 'ч', 'с', 'м', 'и', 'т', 'ь', 'б', 'ю', '.', '▲', 'Shift'],
-//     ['Ctrl', 'Win', 'Alt', 'Space', 'Alt', '◄', '▼', '►', 'Ctrl']];
-
-//   const arrayOfKeys = [arrayOfKeysEN];
-
-//   const keyboard = document.querySelector('keyboard');
-//   for (let i = 0; i < arrayOfKeys.length; i += 1) {
-//     for (let j = 1; j < arrayOfKeys[i].length; j += 1) {
-//       const keyboardRow = document.createElement('div');
-//       keyboardRow.classList.add('keyboard__row');
-//       for (let k = 0; k < arrayOfKeys[i][j].length; k += 1) {
-//         const key = document.createElement('div');
-//         key.classList.add('keyboard__key', 'keyboard__key_size_s');
-//         // key.innerHTML = arrayOfKeys[i][j][k];
-//         keyboardRow.append(key);
-//       }
-//       keyboard.append(keyboardRow);
-//     }
-//   }
-// };
-
-// renderKeys();
